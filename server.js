41478921b5db0d673d78a1c3c4b35336f92f4ab2
lucky-shadowhunter <shadowhunter.lucky@proton.me>
2025-03-04 06:04:44 -0800

    delete unnecessary files

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const https = require("https");
 const WebSocket = require("ws");
 const axios = require("axios");
 const app = express();
